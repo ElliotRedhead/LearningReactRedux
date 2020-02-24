4b810bf6a84ca30823bc393106e4a89d1e143bae
Elliot Redhead <elliot.redhead@gmail.com>
2020-02-24 20:29:32 +0000

    Created example pages.

---
+++ my-app/src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-dom";
+
+const HomePage = () => (
+    <div className="jumbotron">
+        <h1>FirstReact HomePage</h1>
+        <p>React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.</p>
+        <Link to="about" className="btn btn-primary btn-lg">
+        Learn More
+        </Link>
+        </div>
+)
+
+export default HomePage;
