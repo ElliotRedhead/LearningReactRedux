cc2103e603fafabe7ca1e01d10480c9692080910
Elliot Redhead <elliot.redhead@gmail.com>
2020-02-25 10:00:08 +0000

    Added courses page and default home.

--- my-app/src/components/courses/CoursesPage.js
+++ my-app/src/components/courses/CoursesPage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // Only using a class here for stateful implementation and to learn both syntaces.
 
-class CoursePage extends React.Component {
+class CoursesPage extends React.Component {
     render() {
         return <h2>Courses</h2>;
     }
