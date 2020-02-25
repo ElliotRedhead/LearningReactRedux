8a4c63b6724750d131416df13aa9cb15ae86e5d2
Elliot Redhead <elliot.redhead@gmail.com>
2020-02-25 09:56:45 +0000

    Git init, created new page.

---
+++ my-app/src/components/courses/CoursesPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Only using a class here for stateful implementation and to learn both syntaces.
+
+class CoursePage extends React.Component {
+    render() {
+        return <h2>Courses</h2>;
+    }
+}
+
+export default CoursesPage;
