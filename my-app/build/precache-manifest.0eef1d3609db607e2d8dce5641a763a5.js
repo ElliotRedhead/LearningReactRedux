44eeb2046e2e704130e32c6c0545250e05f7d156
Elliot Redhead <elliot.redhead@gmail.com>
2020-02-25 07:19:04 +0000

    Created github pages app version.

---
+++ my-app/build/precache-manifest.0eef1d3609db607e2d8dce5641a763a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe0d02b9a9d04f24b031015109104a0",
+    "url": "/FirstReact/index.html"
+  },
+  {
+    "revision": "95d89ca34b922e4e772f",
+    "url": "/FirstReact/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "da3dfe6e066c20d8e379",
+    "url": "/FirstReact/static/css/main.63afb63c.chunk.css"
+  },
+  {
+    "revision": "95d89ca34b922e4e772f",
+    "url": "/FirstReact/static/js/2.96955b84.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/FirstReact/static/js/2.96955b84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3dfe6e066c20d8e379",
+    "url": "/FirstReact/static/js/main.c5b806ee.chunk.js"
+  },
+  {
+    "revision": "fe6698bab43cc5211a54",
+    "url": "/FirstReact/static/js/runtime-main.d9c89651.js"
+  }
+]);
