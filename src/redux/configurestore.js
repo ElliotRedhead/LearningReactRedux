63fec580d799f46f08f3fbd0e1290703688cb7cc
unknown <elliot.redhead@gmail.com>
2020-06-04 17:01:46 +0100

    Added interaction with Redux store in browser.

--- src/redux/configurestore.js
+++ src/redux/configurestore.js
@@ -1,11 +1,19 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "/reducers";
 import reduxImmutatableStateInvariant from "redux-immutable-state-invariant";
 
+/**
+ * Configures the redux store.
+ * Adds support for Redux dev tools, allowing interaction with Redux store in the browser.
+ * @param {*} initialState The initial state of the Redux store.
+ * @returns The updated store state.
+ */
 export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutatableStateInvariant())
+    composeEnhancers(applyMiddleware(reduxImmutatableStateInvariant()))
   );
 }
