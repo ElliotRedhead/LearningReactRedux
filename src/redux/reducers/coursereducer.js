d3a1334e0eeb500c7864c882faa4661db066c2ee
unknown <elliot.redhead@gmail.com>
2020-06-04 16:38:16 +0100

    Created course reducer.

---
+++ src/redux/reducers/coursereducer.js
@@ -0,0 +1,13 @@
+/**
+ * Updates the redux store by adding the new action passed in action.course to store.
+ * Whatever is returned from the reducer becomes the new state for that reducer.
+ * If the reducer receives an action that it doesn't care about, it should return the unchanged state.
+ */
+export default function courseReducer(state = [], action) {
+  switch (action.type) {
+    case "CREATE_COURSE":
+      return [...state, { ...action.course }];
+    default:
+      return state;
+  }
+}
