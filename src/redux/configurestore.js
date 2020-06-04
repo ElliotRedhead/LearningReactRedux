8098ce84c11dd7088ce630ef7fd4716071a5c8ec
unknown <elliot.redhead@gmail.com>
2020-06-04 17:18:20 +0100

    Correct import paths.

--- src/redux/configurestore.js
+++ src/redux/configurestore.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "/reducers";
+import rootReducer from "./reducers";
 import reduxImmutatableStateInvariant from "redux-immutable-state-invariant";
 
 /**
