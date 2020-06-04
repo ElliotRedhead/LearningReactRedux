461acc8077ba87b70e38cd952f62ea12b480beac
unknown <elliot.redhead@gmail.com>
2020-06-04 17:21:50 +0100

    Linting corrections.

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,11 +4,15 @@ import { Link } from "react-router-dom";
 const HomePage = () => (
   <div className="jumbotron">
     <h1>FirstReact HomePage</h1>
-    <p>React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.</p>
+    <p>
+      React makes it painless to create interactive UIs. Design simple views for
+      each state in your application, and React will efficiently update and
+      render just the right components when your data changes.
+    </p>
     <Link to="about" className="btn btn-primary btn-lg">
-        Learn More
+      Learn More
     </Link>
   </div>
-)
+);
 
 export default HomePage;
