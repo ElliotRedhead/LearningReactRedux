b82725720afd73471649d0cc37cb9afebfaebe25
unknown <elliot.redhead@gmail.com>
2020-06-04 13:38:38 +0100

    Setup form to create new course page.

--- my-app/src/components/courses/CoursesPage.js
+++ my-app/src/components/courses/CoursesPage.js
@@ -3,8 +3,28 @@ import React from "react";
 // Only using a class here for stateful implementation and to learn both syntaces.
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: {
+        title: "",
+      },
+    };
+  }
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
