a815278dc89a1af517bdc0ecc3138731cefb1f86
unknown <elliot.redhead@gmail.com>
2020-06-05 17:27:46 +0100

    Removed inline manage course form.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -7,49 +7,14 @@ import {bindActionCreators} from "redux";
 // Only using a class here for stateful implementation and to learn both syntaces.
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: "",
-    },
-  };
-
-  /**
-   * Copy current course from state, set new title passed from event target value.
-   * Sets state to pass.
-   *
-   * A class field is used to bind 'this' to instance rather than change handler.
-   * New object is created with object spread (values on right overwrite values on left).
-   * @param {object} event The user's input.
-   */
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  /**
-   * Dispatches an action on Submit event.
-   * @param {object} event The user's input.
-   */
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
         {this.props.courses.map((course) => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
