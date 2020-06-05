4695d7a32c48a267e5cde1e1b82fbeb4d2c35ae1
unknown <elliot.redhead@gmail.com>
2020-06-05 20:05:47 +0100

    Created actions for load courses & load success.

--- src/redux/actions/courseactions.js
+++ src/redux/actions/courseactions.js
@@ -1,4 +1,5 @@
 import * as types from "./actiontypes";
+import * as courseApi from "../../api/courseApi";
 
 /**
  * First action creator.
@@ -8,4 +9,28 @@ import * as types from "./actiontypes";
  */
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course };
+}
+
+/**
+ * Declares action creator.
+ * @param {object} courses The chosen payload, the existing courses.
+ * @return {object} The success action.
+ */
+export function loadCourseSuccess(courses){
+  return {type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+/**
+ * First thunk, every thunk returns function that takes dispatch as arg.
+ * Redux thunk injects dispatch for us, so calling code looks same for async and sync calls.
+ * @return {function} 
+ */
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi.getCourses().then(courses => {
+      dispatch(loadCourseSuccess(courses));
+    }).catch(error => {
+      throw error;
+    });
+  };
 }
