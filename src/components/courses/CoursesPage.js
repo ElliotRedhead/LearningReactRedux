79beaebd0d2bc119c1b2941f564c89938cca99e6
unknown <elliot.redhead@gmail.com>
2020-06-06 15:50:16 +0100

    Reduced verbosity by with constant declarations.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -12,13 +12,14 @@ import CourseList from "./CourseList";;
  */
 class CoursesPage extends React.Component {
   componentDidMount(){
-    if (this.props.courses.length === 0){
-      this.props.actions.loadCourses().catch(error => {
+    const { courses, authors, actions} = this.props;
+    if (courses.length === 0){
+      actions.loadCourses().catch(error => {
         alert("Loading courses failed" + error);
       });
     }
-    if(this.props.authors.length === 0){
-      this.props.actions.loadAuthors().catch(error => {
+    if(authors.length === 0){
+      actions.loadAuthors().catch(error => {
         alert("Loading authors failed" + error);
       });
     }
