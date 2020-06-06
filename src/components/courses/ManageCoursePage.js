01f8df8f5c72fc241ef95a9823bc4d08fbe890cc
unknown <elliot.redhead@gmail.com>
2020-06-06 16:44:40 +0100

    mapDispatchToProps converted to object form.

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -3,21 +3,20 @@ import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseactions";
 import * as authorActions from "../../redux/actions/authoractions";
 import PropTypes from "prop-types";
-import {bindActionCreators} from "redux";
 
 /**
  * Creates course page component, utilising courses and authors properties.
  */
 class ManageCoursePage extends React.Component {
   componentDidMount(){
-    const { courses, authors, actions} = this.props;
+    const { courses, authors, loadAuthors, loadCourses} = this.props;
     if (courses.length === 0){
-      actions.loadCourses().catch(error => {
+      loadCourses().catch(error => {
         alert("Loading courses failed" + error);
       });
     }
     if(authors.length === 0){
-      actions.loadAuthors().catch(error => {
+      loadAuthors().catch(error => {
         alert("Loading authors failed" + error);
       });
     }
@@ -35,7 +34,8 @@ class ManageCoursePage extends React.Component {
 ManageCoursePage.propTypes = {
   authors: PropTypes.array.isRequired,
   course: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
 };
 
 /**
@@ -56,16 +56,10 @@ function mapStateToProps(state) {
  * Determines what actions are available to the component.
  * New actions can be passed to allow component to make new requests.
  * bindActionCreators converts an object with action creators as values into an on object with each action creator wrapped into a dispatch call.
- * @param {function} dispatch A function of the Redux store, called to dispatch an action and trigger a state change, interaction with the store is only ever available with connect.
- * @returns {function} Dispatches an action.
  */
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: {
-      loadCourses:bindActionCreators(courseActions.loadCourses, dispatch),
-      loadAuthors:bindActionCreators(authorActions.loadAuthors, dispatch)
-    }
-  };
-}
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
