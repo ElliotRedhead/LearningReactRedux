29a85bcf15daa5e9bdfbc1b2e9e06dba14d1d4a3
unknown <elliot.redhead@gmail.com>
2020-06-06 11:58:58 +0100

    Created author actions.

---
+++ src/redux/actions/authoractions.js
@@ -0,0 +1,29 @@
+import * as types from "./actiontypes";
+import * as authorApi from "../../api/authorApi";
+
+
+/**
+ * Declares action creator.
+ * @param {object} authors The chosen payload, the existing authors.
+ * @return {object} The success action.
+ */
+export function loadAuthorsSuccess(authors){
+  return {type: types.LOAD_AUTHORS_SUCCESS, authors};
+}
+
+/**
+ * Thunk, every thunk returns function that takes dispatch as arg.
+ * Redux thunk injects dispatch for us, so calling code looks same for async and sync calls.
+ * Returns a list of authors if successful, 
+ * @return {function} 
+ */
+export function loadAuthors() {
+  return function (dispatch) {
+    return authorApi.getAuthors()
+      .then(authors => {
+        dispatch(loadAuthorsSuccess(authors));})
+      .catch(error => {
+        throw error;
+      });
+  };
+}
