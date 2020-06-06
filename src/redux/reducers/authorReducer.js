17572c1a4e8e92477c8cf60966de98bc66274157
unknown <elliot.redhead@gmail.com>
2020-06-06 12:19:09 +0100

    Created author reducer.

---
+++ src/redux/reducers/authorReducer.js
@@ -0,0 +1,17 @@
+import * as types from "../actions/actiontypes";
+/**
+ * Updates the redux store by adding the new action passed in action.course to store.
+ * Whatever is returned from the reducer becomes the new state for that reducer.
+ * If the reducer receives an action that it doesn't care about, it should return the unchanged state.
+ * @param {array} state The properties of a component.
+ * @param {object} action The payload of information sent from data from application the store.
+ * @returns {object} The updated state.
+ */
+export default function authorReducer(state = [], action) {
+  switch (action.type) {
+  case types.LOAD_AUTHORS_SUCCESS:
+    return action.authors;
+  default:
+    return state;
+  }
+}
