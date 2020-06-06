387c0f72b1d4cc4396f3d7f6781500f85bb4be64
unknown <elliot.redhead@gmail.com>
2020-06-06 12:24:58 +0100

    Updated root reducer to reference new reducer.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import courses from "./coursereducer";
+import authors from"./authorreducer";
 
 const rootReducer = combineReducers({
   courses,
+  authors
 });
 
 export default rootReducer;
