5a2b836487cee5f0afe8ebb2ffcea80f3c221380
unknown <elliot.redhead@gmail.com>
2020-06-09 17:57:57 +0100

    Added placeholder loading spinner.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import {bindActionCreators} from "redux";
 import CourseList from "./CourseList";
 import { Redirect } from "react-router-dom";
+import Spinner from "../common/Spinner";
 // Only using a class here for stateful implementation and to learn both syntaces.
 
 /**
@@ -34,6 +35,7 @@ class CoursesPage extends React.Component {
       <>
         {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
+        <Spinner />
         <button 
           style={{marginBottom:20}}
           className="btn btn-primary add-course"
