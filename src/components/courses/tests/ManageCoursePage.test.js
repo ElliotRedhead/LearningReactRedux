e0a34f158e4ebb4f9a4bda94fa70a2f1bc02ebb0
unknown <elliot.redhead@gmail.com>
2020-06-12 17:11:34 +0100

    Created test for invalid form submission handling.

---
+++ src/components/courses/tests/ManageCoursePage.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { mount } from "enzyme";
+import { authors, newCourse, courses } from "../../../tools/mockData";
+import ManageCoursePage from "../ManageCoursePage";
+
+function render(args){
+  const defaultProps = {
+    authors,
+    courses,
+    // Passed from React Router in real app, so stubbed in for test.
+    history:{},
+    saveCourse: jest.fn(),
+    loadAuthors: jest.fn(),
+    loadCourses: jest.fn(),
+    course: newCourse,
+    match: {}
+  };
+  const props= {...defaultProps, ...args};
+  return mount(<ManageCoursePage {...props} />);
+}
+
+it("sets error when attempting to save an empty title field", () => {
+  const wrapper = render();
+  // Simulate can be used to simulate user input.
+  wrapper.find("form").simulate("submit");
+  const error = wrapper.find(".alert").first();
+  expect(error.text()).toBe("Title is required.");
+});
